4923d22020d8b8fa40ea8c1bb9b49bd13bc735d1
akeim sutherland <akeim sutherland>
2016-10-13 23:03:04 -0500

    Adding search

--- js/cMap.js
+++ js/cMap.js
@@ -7,6 +7,6 @@ $(document).ready(function() {
       "Google": 'http://placehold.it/250x250'
     }
   });
-  chosen = $("#autocomplete-input").val();
+  chosen = $(".autocomplete").text();
   Console.log(chosen);
 });
