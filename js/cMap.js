382cb6df97e0b4040774fa2484a227f9b012d9a9
akeim sutherland <akeim sutherland>
2016-10-16 10:19:09 -0500

    getting data from database

--- js/cMap.js
+++ js/cMap.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
   var choice = function(){
     data = database.ref("/stores");
     data.on("value",function(snapshot){
-      if(chosen in snapshot.val()){
+      if("flour" in snapshot.val()){
       console.log(snapshot.val());
     }else{
       console.log("Not Found!");
